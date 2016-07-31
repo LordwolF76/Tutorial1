06c5badd66f4755ca75e3f18d8887c4d1b01f964
LordwolF76 <lordwolf76@gmail.com>
2016-07-31 15:40:08 -0700

    Initial commit

---
+++ src/Application.java
@@ -0,0 +1,34 @@
+/**
+ * Created by LordwolF on 7/31/2016.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int myNumber = 88;
+        /* 32 bit number */
+        short myShort = 847;
+        //16 bit number
+        long myLong = 988885;
+        //64bit number
+        double myDouble = 3.1415;
+        //floating point integer
+
+        char myChar = 'y';
+        /* unicode single quotes */
+        boolean myBoolean = true;
+        /* boolean */
+        byte myByte = 127;
+        /* a byte max of 127 */
+
+
+        System.out.println(myNumber);
+        System.out.println(myShort);
+        System.out.println(myLong);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(myByte);
+
+    }
+}
